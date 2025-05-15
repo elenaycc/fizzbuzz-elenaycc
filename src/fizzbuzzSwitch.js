243c61ffae61d294f81a80a6eddd29f52d2d72af
elenaycc <elenaycelik@gmail.com>
2025-05-15 13:53:15 +0300

    success

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,19 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+  let output = "";
+  if (num % 3 === 0) output += "Fizz";
+  if (num % 5 === 0) output += "Buzz";
+
+  switch (output) {
+    case "Fizz":
+      return "Fizz";
+    case "Buzz":
+      return "Buzz";
+    case "FizzBuzz":
+      return "FizzBuzz";
+    default:
+      return num;
+      break;
+  }
 }
 
 module.exports = fizzbuzzSwitch;
